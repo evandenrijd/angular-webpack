b5efd00a088ebe8dab205c8961330444ff010530
van <evandenrijd@gmail.com>
2016-08-31 11:03:47 +0200

    style(indent): indent the concours layout

--- src/app/common/models/concours.module.js
+++ src/app/common/models/concours.module.js
@@ -12,112 +12,112 @@ import '../meta.module';
     let model = null; //copy of the data used in formly
 
     let layoutEdit = [
-          {
-            className: 'layout-row',
-            fieldGroup: [
-              { className: 'flex-33', key: 'code' },
-              { className: 'flex', key: 'title' }
-            ]
-          },
+      {
+        className: 'layout-row',
+        fieldGroup: [
+          { className: 'flex-33', key: 'code' },
+          { className: 'flex', key: 'title' }
+        ]
+      },
 
-          {
-            className: 'layout-row',
-            fieldGroup: [
-              { className: 'flex', key: 'description',
-                type: 'textarea',
-                templateOptions: {
-                  grow: false,
-                  rows: 3
+      {
+        className: 'layout-row',
+        fieldGroup: [
+          { className: 'flex', key: 'description',
+            type: 'textarea',
+            templateOptions: {
+              grow: false,
+              rows: 3
+            },
+          }
+        ]
+      },
+
+      {
+        className: 'layout-row',
+        fieldGroup: [
+          { className: 'flex', key: 'comments',
+            type: 'textarea',
+            templateOptions: {
+              grow: false,
+              rows: 2
+            },
+          }
+        ]
+      },
+
+      {
+        className: 'layout-row',
+        fieldGroup: [
+          { className: 'flex', key: 'question' },
+          { className: 'flex', key: 'answer' },
+        ]
+      },
+
+      {
+        className: 'layout-row',
+        fieldGroup: [
+          { className: 'flex-20', key: 'winners' },
+          { className: 'flex',
+            key: 'startDate',
+            validators: {
+              checkEndDate: {
+                expression: function(viewValue, modelValue, scope) {
+                  let value = modelValue || viewValue;
+                  if (value) value = value.setHours(0,0,0,0); //only compare date not time
+                  if (value) {
+                    let drawingDate = scope.model.drawingDate.setHours(0,0,0,0);
+                    let endDate = scope.model.endDate.setHours(0,0,0,0);
+                    if (endDate && drawingDate) endDate = endDate < drawingDate?drawingDate:endDate;
+                    return value < endDate;
+                  }
+                  return true;
                 },
+                message: '"Should be less then end and/or drawing date."'
               }
-            ]
+            }
           },
-
-          {
-            className: 'layout-row',
-            fieldGroup: [
-              { className: 'flex', key: 'comments',
-                type: 'textarea',
-                templateOptions: {
-                  grow: false,
-                  rows: 2
+          { className: 'flex',
+            key: 'endDate',
+            validators: {
+              checkDrawingDate: {
+                expression: function(viewValue, modelValue, scope) {
+                  let value = modelValue || viewValue;
+                  if (value) value = value.setHours(0,0,0,0); //only compare date not time
+                  if (scope.model.drawingDate) {
+                    return value <= scope.model.drawingDate.setHours(0,0,0,0);
+                  }
+                  return true;
                 },
+                message: '"Should be less then or equal to drawing date."'
               }
-            ]
+            }
           },
-
-          {
-            className: 'layout-row',
-            fieldGroup: [
-              { className: 'flex', key: 'question' },
-              { className: 'flex', key: 'answer' },
-            ]
-          },
-
-          {
-            className: 'layout-row',
-            fieldGroup: [
-              { className: 'flex-20', key: 'winners' },
-              { className: 'flex',
-                key: 'startDate',
-                validators: {
-                  checkEndDate: {
-                    expression: function(viewValue, modelValue, scope) {
-                      let value = modelValue || viewValue;
-                      if (value) value = value.setHours(0,0,0,0); //only compare date not time
-                      if (value) {
-                        let drawingDate = scope.model.drawingDate.setHours(0,0,0,0);
-                        let endDate = scope.model.endDate.setHours(0,0,0,0);
-                        if (endDate && drawingDate) endDate = endDate < drawingDate?drawingDate:endDate;
-                        return value < endDate;
-                      }
-                      return true;
-                    },
-                    message: '"Should be less then end and/or drawing date."'
+          { className: 'flex',
+            key: 'drawingDate',
+            validators: {
+              checkDrawingDate: {
+                expression: function(viewValue, modelValue, scope) {
+                  let value = modelValue || viewValue;
+                  if (value) value = value.setHours(0,0,0,0); //only compare date not time
+                  if (scope.model.endDate) {
+                    return value >= scope.model.endDate.setHours(0,0,0,0);
                   }
-                }
-              },
-              { className: 'flex',
-                key: 'endDate',
-                validators: {
-                  checkDrawingDate: {
-                    expression: function(viewValue, modelValue, scope) {
-                      let value = modelValue || viewValue;
-                      if (value) value = value.setHours(0,0,0,0); //only compare date not time
-                      if (scope.model.drawingDate) {
-                        return value <= scope.model.drawingDate.setHours(0,0,0,0);
-                      }
-                      return true;
-                    },
-                    message: '"Should be less then or equal to drawing date."'
-                  }
-                }
-              },
-              { className: 'flex',
-                key: 'drawingDate',
-                validators: {
-                  checkDrawingDate: {
-                    expression: function(viewValue, modelValue, scope) {
-                      let value = modelValue || viewValue;
-                      if (value) value = value.setHours(0,0,0,0); //only compare date not time
-                      if (scope.model.endDate) {
-                        return value >= scope.model.endDate.setHours(0,0,0,0);
-                      }
-                      return true;
-                    },
-                    message: '"Should be bigger then or equal to end date."'
-                  }
+                  return true;
                 },
-              },
-            ]
+                message: '"Should be bigger then or equal to end date."'
+              }
+            },
           },
+        ]
+      },
 
-          {
-            className: 'layout-row',
-            fieldGroup: [
-              { className: 'flex', key: 'mailWinners' },
-            ]
-          },
+      {
+        className: 'layout-row',
+        fieldGroup: [
+          { className: 'flex', key: 'mailWinners' },
+        ]
+      },
 
       {
         className: 'layout-row',
@@ -126,36 +126,36 @@ import '../meta.module';
         ]
       },
 
-          {
-            className: 'layout-row',
-            fieldGroup: [
-              { className: 'flex', key: 'admins' },
-              { className: 'flex', key: 'drawingAdmin' },
-              { className: 'flex', key: 'creationAdmin' },
-            ]
-          },
+      {
+        className: 'layout-row',
+        fieldGroup: [
+          { className: 'flex', key: 'admins' },
+          { className: 'flex', key: 'drawingAdmin' },
+          { className: 'flex', key: 'creationAdmin' },
+        ]
+      },
 
-          {
-            className: 'layout-row',
-            fieldGroup: [
-              { className: 'flex-33', key: 'exclusionRule' },
-              { className: 'flex', key: 'link' },
-            ]
-          },
+      {
+        className: 'layout-row',
+        fieldGroup: [
+          { className: 'flex-33', key: 'exclusionRule' },
+          { className: 'flex', key: 'link' },
+        ]
+      },
 
-          {
-            className: 'layout-row layout-align-end-center',
-            fieldGroup: [
-              { className: 'flex-20', key: 'status' },
-              { className: 'flex-20', key: 'creationDate',
-                templateOptions: {
-                  disabled: true,
-                },
-              },
-            ]
+      {
+        className: 'layout-row layout-align-end-center',
+        fieldGroup: [
+          { className: 'flex-20', key: 'status' },
+          { className: 'flex-20', key: 'creationDate',
+            templateOptions: {
+              disabled: true,
+            },
           },
+        ]
+      },
 
-        ];
+    ];
 
 
     let getTitle = function() {
